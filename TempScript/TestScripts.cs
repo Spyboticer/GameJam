21ac6af86675bc80c2bad94538903d19dfb48299
Spyboticer <43469665+Spyboticer@users.noreply.github.com>
2023-07-31 17:21:09 +0200

    Added Test unode script upload

---
+++ TempScript/TestScripts.cs
@@ -0,0 +1,10 @@
+#pragma warning disable
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TestScripts : MonoBehaviour {
+
+	private void Start() {
+		Debug.Log("Hello World");
+	}
+}
